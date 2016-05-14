59edcb06512b818be1b3b03248bb3bd4340c55c7
beisenpress <ben@eisenpress.com>
2016-05-13 23:24:55 -0500

    Initial Commit with data and header of program

---
+++ Homework_5.R
@@ -0,0 +1,12 @@
+#### 37301 Data Driven Marketing ######
+#### Homework 5 #######
+
+library(psych)
+library(lattice)
+library(plyr)
+
+setwd("/Users/ben/Dropbox/Chicago Booth/37103 Data Driven Marketing/Homework 5/37103-HW5")
+load("Detailing.RData")
+
+########################################################
+################## Question 1 ##########################
